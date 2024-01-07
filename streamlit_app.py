6e7f1ce075bf11d15b08afefb2abd1580bf0b15b
Shaokun Zhang <58543556+szhan872@users.noreply.github.com>
2024-01-07 13:02:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,58 +27,7 @@ with st.sidebar:
     # For example, links or additional instructions
     api_key = st.text_input("Enter your API key", type="password")
     st.write("We require a GPT-4 Turbo API key, specifically the model gpt-4-1106-preview. Please note that usage may incur charges.")
-
-# getting a novelty score with tfidf
-# def get_tfidf_novelty(problem: str, solution: str):
-#     try:
-#         # Open the file using a context manager
-#         with open('AI EarthHack Dataset.csv', 'r') as file:
-#             # Read the file contents
-#             documents = pd.read_csv('AI EarthHack Dataset.csv', encoding = 'ISO-8859-1')
-
-#     except FileNotFoundError:
-#         print("File not found.")
-#         return False
-
-#     # convert columns to string
-#     documents['problem'] = documents['problem'].astype(str)
-#     documents['solution'] = documents['solution'].astype(str)
-
-#     documents = documents.drop('id', axis=1)
-#     documents = documents.apply(lambda row: row['problem'] + " " + row['solution'], axis=1).tolist()
-
-#     # new statement is from user input
-#     new_statement = problem + " " + solution
-
-#     # TF-IDF Vectorizer
-#     # TfidfVectorizer is equivalent to CountVectorizer followed by TfidfTransformer.
-#     vectorizer = TfidfVectorizer()
-#     tfidf_matrix = vectorizer.fit_transform(documents)
-#     cosine_similarities = cosine_similarity(tfidf_matrix)
-
-#     # Flatten the upper triangle of the similarity matrix and filter out self-similarities (value of 1)
-#     upper_triangle = cosine_similarities[np.triu_indices_from(cosine_similarities, k=1)]
-
-#     ## Calculate the tfidf for the new input
-#     new_statement_tfidf = vectorizer.transform([new_statement])
-
-#     # Compute cosine similarity between the new statement and all documents in the dataset
-#     cosine_similarities = cosine_similarity(new_statement_tfidf, tfidf_matrix)
-
-#     # Heuristic threshold setting
-#     mean_similarity = np.mean(upper_triangle)
-#     std_dev_similarity = np.std(upper_triangle)
-#     threshold = mean_similarity + 2 * std_dev_similarity  # i.e.: mean + 2*std_dev
-
-#     # Find the maximum similarity (you can also use other metrics like average similarity)
-#     max_similarity = np.max(cosine_similarities)
-
-#     # Evaluate novelty. Low similarity indicates high novelty with respect to the given dataset
-#     if max_similarity < 0.5:
-#         return max_similarity, True
-#     else:
-#         return max_similarity, False
-
+    
     
 def emoji():
     rain(

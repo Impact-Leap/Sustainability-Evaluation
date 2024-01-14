d402f81611c21b8d5870df7e3a5f0d86371ec048
HelpMeCodePls <64028614+HelpMeCodePls@users.noreply.github.com>
2024-01-14 16:36:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,35 +181,35 @@ if input_method == 'Manual Input':
         st.metric(label="##### 📈Average Total Raised", value=f"${avg_total_raised:,}")
 
     st.write('<br><br>', unsafe_allow_html=True)
-
-# Custom CSS to inject contained style
-st.markdown(
-    """
-    <style>
-    .box {
-        border: 2px solid #eee;
-        border-radius: 5px;
-        padding: 10px;
-        margin: 10px 0;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-    # Box for metrics
-    with st.container():
-        st.markdown('<div class="box">', unsafe_allow_html=True)
+    
+    # Custom CSS to inject contained style
+    st.markdown(
+        """
+        <style>
+        .box {
+            border: 2px solid #eee;
+            border-radius: 5px;
+            padding: 10px;
+            margin: 10px 0;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
+        # Box for metrics
+        with st.container():
+            st.markdown('<div class="box">', unsafe_allow_html=True)
+            
+            col1, col2 = st.columns(2)
         
-        col1, col2 = st.columns(2)
-    
-        with col1:
-            st.metric(label="Average Number of Competitors", value=avg_num_competitors)
-    
-        with col2:
-            st.metric(label="Average Total Raised", value=f"${avg_total_raised:,}")
-    
-        st.markdown('</div>', unsafe_allow_html=True)    
+            with col1:
+                st.metric(label="Average Number of Competitors", value=avg_num_competitors)
+        
+            with col2:
+                st.metric(label="Average Total Raised", value=f"${avg_total_raised:,}")
+        
+            st.markdown('</div>', unsafe_allow_html=True)    
     
     for category in df_cat['Category'].unique():
     # Filter the DataFrame for the current category
